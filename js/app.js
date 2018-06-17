927e751367c0c8edfe75b5d83a6efb923df65a98
Sum Tsui <sumtsui@gmail.com>
2018-06-17 21:43:55 +0800

    new

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const twit = require('./config');
 
-twit.get('statuses/user_timeline', { screen_name: 'sumtsui', count: 5 }, function (err, data, response) {
+twit.get('statuses/user_timeline', { screen_name: 'sumtsui' }, function (err, data, response) {
     if (err) console.log('fucking error!!', err);
     else console.log('my followers: ', data);
 });
